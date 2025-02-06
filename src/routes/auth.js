7e5988689e3e6eb86ba72a6b151675baa9f1accc
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-06 20:29:27 -0300

    :sparkles: feat: Rota de Login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -22,4 +22,23 @@ routerAuth.post("/register", async (req, res) => {
   }
 });
 
+routerAuth.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = routerAuth;
