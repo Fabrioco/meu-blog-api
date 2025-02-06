f3cfc65ec4d8be971796a994c1c8cde480280f43
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-06 15:47:12 -0300

    :sparkles: feat: Modelo de usuario

---
+++ src/models/user.js
@@ -0,0 +1,25 @@
+const database = require("../config/db");
+const { DataTypes } = require("sequelize");
+
+const User = database.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
